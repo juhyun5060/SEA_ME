7d8a1253971d7cf69e5d4670390466be22d52fad
Juhyun <s2019w09@e-mirim.hs.kr>
2020-11-12 19:37:46 +0900

    add Activity

---
+++ app/src/main/java/kr/hs/emirim/s2019w09/seame/LoadingActivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.s2019w09.seame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}
