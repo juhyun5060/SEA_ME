c2bcec8b0f00cdef94e7d87f45a3fb7e4fb6dda6
Juhyun <s2019w09@e-mirim.hs.kr>
2020-12-05 23:01:03 +0900

    edit LoadingActivity

--- app/src/main/java/kr/hs/emirim/s2019w09/seame/LoadingActivity.kt
+++ app/src/main/java/kr/hs/emirim/s2019w09/seame/LoadingActivity.kt
@@ -1,11 +1,21 @@
 package kr.hs.emirim.s2019w09.seame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class LoadingActivity : AppCompatActivity() {
+
+    val SPLASH_TIME_OUT : Long = 3000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
+
+        Handler().postDelayed( {
+            startActivity(Intent(this, ResultActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 }
